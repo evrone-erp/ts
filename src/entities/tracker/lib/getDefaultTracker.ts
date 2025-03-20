52428209e8f92805714b1bf7b99539e816757cc1
Olga Popova <popova_oa@evrone.team>
2025-03-20 19:10:59 +0500

    [YTS-100] support yandex cloud organization id

--- src/entities/tracker/lib/getDefaultTracker.ts
+++ src/entities/tracker/lib/getDefaultTracker.ts
@@ -1,6 +1,8 @@
 import { Tracker, TYandexTrackerConfig } from 'entities/tracker/model/types';
 import { getTrackerIdFromName } from 'entities/tracker/lib/getTrackerIdFromName';
 
+const yandex360Regex = /^\d+$/;
+
 /**
  * default tracker is always Yandex Tracker, because Timesheet only supported Yandex Tracker in the past
  */
@@ -10,7 +12,7 @@ export const getDefaultTracker = (orgId: string): TYandexTrackerConfig => {
     id: getTrackerIdFromName(name),
     url: 'https://tracker.yandex.ru/',
     name,
-    isCloud: false,
+    isCloud: !yandex360Regex.test(orgId),
     orgId,
     type: Tracker.Yandex,
     // username will be set in useSetTrackerUsername hook when user opens default tracker
