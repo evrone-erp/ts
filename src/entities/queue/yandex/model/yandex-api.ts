016b6d2c0b68ffdfb2e42e4e20b82c88a45d6270
Dmitry Ibragimov <dmitry.ibragimov@lemanapro.ru>
2025-07-03 20:10:24 +0300

    Fix queue limit 50

--- src/entities/queue/yandex/model/yandex-api.ts
+++ src/entities/queue/yandex/model/yandex-api.ts
@@ -1,4 +1,6 @@
+import { fetchAllPages, TFetchAllPagesBaseQueryResult } from 'shared/api';
 import { api } from 'shared/api';
+import { identity } from 'shared/lib/utils';
 import { TGetQueuesParams, TQueue } from 'entities/queue/common/model/types';
 import { getTrackerHeaders } from 'entities/tracker/lib/getTrackerHeaders';
 import { yandexQueueEndpoints } from 'entities/queue/yandex/model/endpoints';
@@ -7,10 +9,18 @@ export const yandexQueueApi = api.injectEndpoints({
   overrideExisting: true,
   endpoints: (build) => ({
     getQueues: build.query<TQueue[], TGetQueuesParams>({
-      query: ({ tracker }) => ({
-        url: yandexQueueEndpoints.queues,
-        headers: getTrackerHeaders(tracker),
-      }),
+      async queryFn({ tracker }, __, ___, fetchWithBQ) {
+        const res = await fetchAllPages(
+          (page) =>
+            fetchWithBQ({
+              url: yandexQueueEndpoints.queues,
+              params: { page, perPage: 100 },
+              headers: getTrackerHeaders(tracker),
+            }) as TFetchAllPagesBaseQueryResult<TQueue[]>,
+          identity,
+        );
+        return res;
+      },
     }),
   }),
 });
